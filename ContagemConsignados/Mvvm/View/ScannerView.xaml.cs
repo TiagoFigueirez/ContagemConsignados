563da71f3b7297dcb658ba68aba28f2ee1aa6963
TIAGO <TIAGO@TIAGO-PC>
2026-01-18 23:26:52 -0300

    Implementando o leitor de codigo de barras

--- ContagemConsignados/Mvvm/View/ScannerView.xaml.cs
+++ ContagemConsignados/Mvvm/View/ScannerView.xaml.cs
@@ -1,4 +1,6 @@
 using ContagemConsignados.Mvvm.ViewModel;
+using ZXing.Net.Maui;
+using ZXing.Net.Maui.Controls;
 
 namespace ContagemConsignados.Mvvm.View;
 
@@ -7,20 +9,26 @@ public partial class ScannerView : ContentPage
 	public ScannerView()
 	{
 		InitializeComponent();
-	}
+
+        cameraView.Options = new BarcodeReaderOptions
+        {
+            Formats = BarcodeFormats.OneDimensional,
+            AutoRotate = true,
+            Multiple = true
+        };
+    }
 
     private void cameraView_BarcodesDetected(object sender, ZXing.Net.Maui.BarcodeDetectionEventArgs e)
     {
 		var codigo = e.Results?.FirstOrDefault()?.Value;
 
-		if(!string.IsNullOrEmpty(codigo) )
-		{
+		if(string.IsNullOrEmpty(codigo) )
+		   return;
+
 			MainThread.BeginInvokeOnMainThread(async () =>
 			{
 				(BindingContext as ScannerViewModel)?.OnBarCodeDetected(codigo);
-
-				await Navigation.PopAsync();
 			});
-		}
+
     }
 }
